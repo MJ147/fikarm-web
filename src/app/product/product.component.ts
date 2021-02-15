50105615df192ab2b11bb47b683d0bcde14aa2c1
Marek Jaczewski <jaczewski147@gmail.com>
2021-02-15 21:55:34 +0100

    add product view

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Product, ProductService } from './../services/product.service';
+import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.less']
+	selector: 'app-product',
+	templateUrl: './product.component.html',
+	styleUrls: ['./product.component.less'],
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent {
+	product$: Observable<Product>;
+	suggestedProducts$: Observable<Product[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	constructor(private _route: ActivatedRoute, private _productService: ProductService) {
+		this.product$ = this._route.paramMap.pipe(
+			map((params) => parseInt(params.get('productId') || '', 10)),
+			filter((productId) => !!productId),
+			switchMap((productId) => this._productService.getById(productId)),
+		);
+		this.suggestedProducts$ = this._productService.getAll();
+	}
 }
